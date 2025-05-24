eaa57130b074762ca483a4fe251fbacb091b4768
liuwei <18682303408@163.com>
2025-05-24 23:50:50 +0800

    compile contain dpdk

---
+++ inc/common/log.h
@@ -0,0 +1,14 @@
+/************************************************
+ * filename: log.h
+ * function:
+ * description:
+ ***********************************************/
+
+#ifndef __LOG_H__
+#define __LOG_H__
+
+#define LOG_DEBUG(format, ...)
+#define LOG_ERROR(format, ...)
+#define LOG_INFO(format, ...)
+
+#endif // __LOG_H__
--- src/main.c
+++ src/main.c
@@ -4,12 +4,30 @@
  * description:
  ***********************************************/
 
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <rte_eal.h>
+
+#include "log.h"
 
 int main(int argc, char *argv[])
 {
+    int ret = 0;
+
+    ret = daemon(0, 0);
+    if (ret != 0) {
+        fprintf(stderr, "daemon failure: %s\n", strerror(errno));
+        return EXIT_FAILURE;
+    }
+
+    ret = rte_eal_init(argc, argv);
+    if (ret != 0) {
+        LOG_ERROR("");
+        return EXIT_FAILURE;
+    }
+
     return EXIT_SUCCESS;
 }
