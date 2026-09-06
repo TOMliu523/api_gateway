ccdca1d5635e217d3f24ef665f882df3567a8a14
liuwei <18682303408@163.com>
2026-09-06 15:49:14 +0000

    Initialize rewrite v1

--- src/main.c
+++ src/main.c
@@ -1,235 +1,26 @@
-/************************************************
+/*****************************************************************************
  * filename: main.c
  * function:
- * description:
- ***********************************************/
+ * description: Application Entry Point
+ ****************************************************************************/
 
-#define _GNU_SOURCE
-#include <time.h>
 #include <errno.h>
 #include <stdio.h>
-#include <fcntl.h>
-#include <sched.h>
 #include <stdlib.h>
-#include <signal.h>
 #include <string.h>
 #include <unistd.h>
-#include <pthread.h>
-#include <sys/types.h>
 
 #include "log.h"
-#include "api.h"
-#include "type.h"
-#include "runtime.h"
-#include "dpdk_init.h"
-#include "dataplane.h"
-
-#define RUN_LOCK_FILE "/run/lock/api_gateway.lock"
-
-static struct root *s_root;
-static int s_fd = -1;
-
-static void _lock_file_free(void)
-{
-    if (s_fd >= 0) {
-        close(s_fd);
-    }
-}
-
-static void single_instance(const char *filename)
-{
-    int fd = -1;
-    int ret = 0;
-    int nbytes = 0;
-    struct flock lock = {0};
-    char buffer[BUFSIZ] = "";
-
-    for (;;) {
-        if (access(filename, F_OK) != 0) {
-            fd = open(filename, O_CREAT | O_EXCL | O_RDWR, 0666);
-            if (fd >= 0) {
-                break;
-            } else if (fd < 0 && errno != EEXIST) {
-                LOG_ERROR("create %s failure: %s", filename, strerror(errno));
-                exit(EXIT_FAILURE);
-            } else {
-                continue;
-            }
-        } else {
-            fd = open(filename, O_RDWR);
-            if (fd >= 0) {
-                break;
-            } else if (fd < 0 && errno != ENOENT) {
-                LOG_ERROR("open %s failure: %s.", filename, strerror(errno));
-                exit(EXIT_FAILURE);
-            } else {
-                continue;
-            }
-        }
-    }
-
-    nbytes = snprintf(buffer, sizeof(buffer), "process number: %u\nprocess startup time: %lu", getpid(), time(NULL));
-    if (nbytes < 0) {
-        LOG_ERROR("snprintf failure: %s", strerror(errno));
-        exit(EXIT_FAILURE);
-    }
-
-    lock.l_type = F_WRLCK;
-    lock.l_whence = SEEK_SET;
-    lock.l_start = nbytes;
-    lock.l_len = 1;
-
-    ret = fcntl(fd, F_SETLK, &lock);
-    if (ret != 0) {
-        LOG_ERROR("lock %s failure: %s", filename, strerror(errno));
-        exit(EXIT_FAILURE);
-    }
-
-    nbytes = write(fd, buffer, nbytes);
-    if (nbytes < 0) {
-        LOG_ERROR("Failure write: %s.", strerror(errno));
-        exit(EXIT_FAILURE);
-    }
-
-    LOG_INFO("Current process ID = %u", getpid());
-    s_fd = fd;
-    atexit(_lock_file_free);
-}
-
-static void signal_process(void)
-{
-    signal(SIGTERM, SIG_IGN);
-    signal(SIGUSR1, SIG_IGN);
-    signal(SIGUSR2, dpdk_cleanup);
-    signal(SIGTTIN, SIG_IGN);
-    signal(SIGTTOU, SIG_IGN);
-    signal(SIGCHLD, SIG_IGN);
-}
-
-static void root_fini(void)
-{
-    struct root *root = NULL;
-
-    if (s_root == NULL) {
-        return;
-    }
-
-    root = s_root;
-    free(root);
-    s_root = NULL;
-}
-
-static INLINE struct root *root_init(struct hw_info *info)
-{
-    int ret = 0;
-    size_t total_size = 0;
-    struct root *root = NULL;
-    struct timespec ts = {0};
-
-    total_size = sizeof(struct root);
-    ret = posix_memalign((void **)&root, CACHE_LINE, total_size);
-    if (ret < 0) {
-        LOG_ERROR("OOM.");
-        exit(EXIT_FAILURE);
-    }
-
-    memset(root, 0, total_size);
-
-    clock_gettime(CLOCK_MONOTONIC, &ts);
-    root->startup_time = ts.tv_sec;
-    memcpy(&root->hw_info, info, sizeof(*info));
-
-    atexit(root_fini);
-    return root;
-}
-
-static void non_dpdk_thread_attr_fini(pthread_attr_t *attr)
-{
-    if (attr != NULL) {
-        pthread_attr_destroy(attr);
-    }
-}
-
-/*
- * Before dpdk_init() is called, the main thread runs on non-DPDK CPUs.
- * After dpdk_init(), the main thread is pinned to a DPDK CPU.
- *
- * Save the CPU affinity attributes before dpdk_init() so that
- * non-dataplane threads created later can inherit them. This prevents
- * those threads from competing with dataplane threads for CPU resources
- * and ensures that they run only on non-DPDK CPUs.
- */
-static int non_dpdk_thread_attr_init(pthread_attr_t *attr)
-{
-    int ret = 0;
-    cpu_set_t mask = {0};
-
-    CPU_ZERO(&mask);
-    ret = sched_getaffinity(0, sizeof(mask), &mask);
-    if (ret < 0) {
-        LOG_ERROR("sched_getaffinity failure.");
-        return -1;
-    }
-
-    ret = pthread_attr_init(attr);
-    if (ret < 0) {
-        LOG_ERROR("pthread_attr_init %s failure.", strerror(errno));
-        return -1;
-    }
-
-    ret = pthread_attr_setaffinity_np(attr, sizeof(mask), &mask);
-    if (ret < 0) {
-        LOG_ERROR("pthread_attr_setaffinity_np %s fialure.", strerror(errno));
-        pthread_attr_destroy(attr);
-        return -1;
-    }
-
-    return 0;
-}
 
 int main(int argc, char *argv[])
 {
     int ret = 0;
-    pthread_t tid = {0};
-    pthread_attr_t attr = {0};
-    struct hw_info info = {0};
 
     ret = daemon(0, 0);
-    if (ret != 0) {
-        LOG_ERROR("daemon failure: %s", strerror(errno));
-        return EXIT_FAILURE;
-    }
-
-    signal_process();
-    single_instance(RUN_LOCK_FILE);
-
-    ret = non_dpdk_thread_attr_init(&attr);
     if (ret < 0) {
+        LOG_ERROR("daemon failure: %s\n", strerror(errno));
         return EXIT_FAILURE;
     }
 
-    ret = dpdk_init(argc, argv, &info);
-    if (ret < 0) {
-        return EXIT_FAILURE;
-    }
-
-    argc -= ret;
-    argv += ret;
-
-    s_root = root_init(&info);
-    ret = pthread_create(&tid, &attr, api_startup, s_root);
-    if (ret != 0) {
-        LOG_ERROR("Startup api thread failure: %s", strerror(ret));
-        return EXIT_FAILURE;
-    }
-
-    ret = pthread_create(&tid, &attr, runtime_state_startup, s_root);
-    if (ret != 0) {
-        LOG_ERROR("Startup running thread failure: %s", strerror(ret));
-        return EXIT_FAILURE;
-    }
-
-    non_dpdk_thread_attr_fini(&attr);
-    dpdk_thread_startup(dp_startup, s_root);
     return EXIT_SUCCESS;
 }
